8ac82477c684d2f00ace3851bfc4ec8be7caf7e1
Mystic Striver <187439614+StriverV@users.noreply.github.com>
2025-04-01 23:59:00 +0600

    Add files via upload

---
+++ Basic_Python/C_Good_Sequence.py
@@ -0,0 +1,20 @@
+n = int(input()) 
+a = list(map(int, input().split()))
+
+freq = {}
+
+for i in range(n):
+    if a[i] in freq:
+        freq[a[i]] += 1 
+    else:
+        freq[a[i]] = 1 
+
+        
+min_num_of_element = 0
+for i,val in freq.items():
+    if (val < i):
+        min_num_of_element += val
+    elif (val > i):
+        min_num_of_element += val - i
+    
+print(min_num_of_element)
