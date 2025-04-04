e9d2908cd4db0992e136e19442b90d76bebc9674
Mystic Striver <187439614+StriverV@users.noreply.github.com>
2025-04-04 08:45:02 +0600

    Update default_arg.py

--- Basic_Python/default_arg.py
+++ Basic_Python/default_arg.py
@@ -1,6 +1,6 @@
 def sum(n1,n2,n3=0,n4=0,n5=0):
-    add = n1+n2+n3
-    return add
-total = sum(1,2,3)
-print('final ',total)
+    return n1+n2+n3   
+ 
+print(sum(1,2)) #1+2+0
+print(sum(1,2,4)) #1+2+4
     
